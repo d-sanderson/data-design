959b11b4cf1f88d95393957de49f8be889181cf7
David Sanderson <dsanderson3@cnm.edu>
2018-10-10 14:40:50 -0600

    Added Files

---
+++ epic/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Site Nav: </h1>
+			<p>
+				<a href="RalphHODLCryptopherson.php">Persona</a>
+				<a href="conceptual-model.php">Conceptual Model</a>
+				<a href="use-case.php">Conceptual Model</a>
+			</p>
+
+	</body>
+</html>
